abbee4c6326613462c66d406990958a2d3581d32
Jesse Ramirez Sosa <jesse@Mac.lan>
2026-01-07 18:35:11 -0600

    Add Python basics and calculator scripts

---
+++ basics.py
@@ -0,0 +1,9 @@
+#Begin
+
+name = input("Enter your name: ")
+age  = int(input("Enter you age: "))
+
+years_to_30 = 30 - age
+
+print(f"Hello {name}")
+print(f"you will turn 30 in {years_to_30} years")
---
+++ calculator.py
@@ -0,0 +1,13 @@
+#Calculate
+
+a = float(input("Enter first number: "))
+b = float (input("Enter second number: "))
+
+print("Sum: ", a + b)
+print("Differencial: ", a / b)
+print("Product: ", a * b)
+
+if b != 0: 
+    print("Divison: ", a / b)
+else:
+    print("Cannot divide by zero")
