308d327de72254111b3f80b5c753ce9316b464ab
Jesse Ramirez Sosa <jesse@Mac.lan>
2026-01-10 00:53:33 -0600

    Refractor loops scipt using function

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-# simple Python refresh script
-name = input("Enter your name: ")
-print(f"Hello, {name}! Welcome to Python.")
+#Clean + professional 
 
-for i in range(5):
-    print(f"{i+1}: Python is fun!")
+def get_number(count):
+    numbers = []
+    for i in range(count):
+        num = float(input(f"Enter number {i+1}: "))
+        numbers.append(num)
+    return numbers  
+
+def calculate_average(numbers):
+    total = 0 
+    for n in numbers:
+        total += n
+    return total / len(numbers)
+
+nums = get_number(5)
+avg =  calculate_average(nums)
+print("Average: ", avg)
